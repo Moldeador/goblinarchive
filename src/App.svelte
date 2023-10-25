33d78bba54ec739648b93fba48739c50d858f2f7
Santi <sontrax@gmail.com>
2023-10-25 16:44:35 +0200

    First draft of the landing page

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,13 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import '@fontsource/unifrakturcook';
+  import '@fontsource-variable/sen';
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+
+  <h1>Goblin 𓃛 Archive</h1>
 
   <div class="card">
     <Counter />
