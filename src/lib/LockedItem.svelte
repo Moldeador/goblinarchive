230ce773dfec55e4be68f83bbd0710a96f09bb27
Santi <sontrax@gmail.com>
2025-03-09 20:11:51 +0100

    LockedItem created

---
+++ src/lib/LockedItem.svelte
@@ -0,0 +1,52 @@
+<script>
+    export let x;
+    export let y;
+    export let width;
+    export let height;
+    export let name;
+    
+    function handleItemClick(){
+        window.location.href = '#/locked/'+name;
+    }
+</script>
+
+
+<div class="painting tooltipholder" on:click={handleItemClick} style='--x:{x}; --y:{y}; --width:{width}; --height:{height}'>
+    <span class="tooltip">name</span>
+</div>
+
+
+<style>
+  .painting{
+    background-color: black;
+    width:calc( var(--width) * 1px );
+    height:calc( var(--height) * 1px );
+    position: absolute;
+    bottom:calc( var(--y) * 1px );
+    left:calc( var(--x) * 1px );
+  }
+
+  .tooltip {
+  visibility: hidden;
+  width: 120px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: -20px;
+  left: 50%;
+  margin-left: -60px;
+  opacity: 0;
+  transition: opacity 2s;
+}
+
+
+.tooltipholder:hover .tooltip {
+  visibility: visible;
+  opacity: 1;
+}
+
+</style>
--- src/routes/Locked.svelte
+++ src/routes/Locked.svelte
@@ -1,19 +1,12 @@
 <script>
-    function handleGoToPainting(){
-        window.location.href = '#/locked/painting';
-    }
+    import LockedItem from "../lib/LockedItem.svelte";
 </script>
 
 <main>
   <div class="gameContainer">
-    <div class="door tooltipholder">
-      <span class="tooltip">Door</span>
-    </div>
 
-      
-    <div class="painting tooltipholder" on:click={handleGoToPainting}>
-      <span class="tooltip">Painting</span>
-    </div>
+    <LockedItem name={"door"} x={200} y={0} width={200} height={400}/>
+    <LockedItem name={"painting"} x={700} y={300} width={300} height={200}/>
   </div>
 
 </main>
@@ -32,46 +25,4 @@
     background-color: white;
     position: relative;
     }
-  .door{
-    background-color: black;
-    width: 200px;
-    height: 400px;
-    border-top-left-radius: 5%;
-    border-top-right-radius: 5%;
-    position: absolute;
-    bottom: 0px;
-    left: 200px;
-  }
-  .painting{
-    background-color: black;
-    width: 300px;
-    height: 200px;
-    border-radius: 5%;
-    position: absolute;
-    bottom: 300px;
-    left: 700px;
-  }
-
-  .tooltip {
-  visibility: hidden;
-  width: 120px;
-  background-color: #555;
-  color: #fff;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px 0;
-  position: absolute;
-  z-index: 1;
-  bottom: -20px;
-  left: 50%;
-  margin-left: -60px;
-  opacity: 0;
-  transition: opacity 2s;
-}
-
-
-.tooltipholder:hover .tooltip {
-  visibility: visible;
-  opacity: 1;
-}
 </style>
