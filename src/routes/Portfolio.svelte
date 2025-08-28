4e3988ac711b03c645d5838308f57a49b8176b1e
Santi <sontrax@gmail.com>
2025-08-28 21:46:39 +0200

    Added the new React app to Portfolio

--- src/routes/Portfolio.svelte
+++ src/routes/Portfolio.svelte
@@ -1,59 +1,73 @@
 <script>
-    import PortfolioItem from "../lib/PortfolioItem.svelte";
+  import PortfolioItem from "../lib/PortfolioItem.svelte";
 
-    let imageFeatherandfox = "/images/featherandfox.png";
-    let imageDndPublications = "/images/dnd-publications.png";
-    let imageApp = "/images/app-roll-for-initiative.png";
-    let imageCV = "/images/CV-image.png";
-    let imagePapers = "/images/papers-3.png";
-    let imagePoems = "/images/poemas.png";
-    let imageMidjourney = "/images/deck-of-many-things.png";
+  let imageFeatherandfox = "/images/featherandfox.png";
+  let imageReact = "/images/card-searcher.png";
+  let imageDndPublications = "/images/dnd-publications.png";
+  let imageApp = "/images/app-roll-for-initiative.png";
+  let imageCV = "/images/CV-image.png";
+  let imagePapers = "/images/papers-3.png";
+  let imagePoems = "/images/poemas.png";
+  let imageMidjourney = "/images/deck-of-many-things.png";
 
-    let linkFeatherandfox = "https://featherandfox.com";
-    let linkDnd = "https://www.dmsguild.com/browse.php?author=Santiago%20Cabrera";
-    let linkApp = "https://rollforinitiative.goblinarchive.com";
-    let linkCV = "/pdfs/cv_goblinarchive.pdf";
-    let linkPapers = "https://inspirehep.net/authors/1607971?ui-citation-summary=true";
-    let linkPoems = "https://buscadbuscadlos.blogspot.com/";
-    let linkMidjourney = "https://santibanti.itch.io/the-deck-of-many-things";
+  let linkFeatherandfox = "https://featherandfox.com";
+  let linkReact = "https://moldeador.github.io/card-searcher-react/";
+  let linkDnd = "https://www.dmsguild.com/browse.php?author=Santiago%20Cabrera";
+  let linkApp = "https://rollforinitiative.goblinarchive.com";
+  let linkCV = "/pdfs/cv_goblinarchive.pdf";
+  let linkPapers =
+    "https://inspirehep.net/authors/1607971?ui-citation-summary=true";
+  let linkPoems = "https://buscadbuscadlos.blogspot.com/";
+  let linkMidjourney = "https://santibanti.itch.io/the-deck-of-many-things";
 </script>
 
 <main>
-
   <div class="top">
     <h2>Santi's Portfolio</h2>
   </div>
 
   <div class="center">
-    <PortfolioItem itemTitle={"Web"} imageSrc={imageFeatherandfox} link={linkFeatherandfox}/>
-    <PortfolioItem itemTitle={"App"} imageSrc={imageApp} link={linkApp}/>
-    <PortfolioItem itemTitle={"Papers"} imageSrc={imagePapers} link={linkPapers}/>
-    <PortfolioItem itemTitle={"D&D"} imageSrc={imageDndPublications} link={linkDnd}/>
-    <PortfolioItem itemTitle={"Poems"} imageSrc={imagePoems} link={linkPoems}/>
-    <PortfolioItem itemTitle={"Itch"} imageSrc={imageMidjourney} link={linkMidjourney}/>
-    <PortfolioItem itemTitle={"Resumé"} imageSrc={imageCV} link={linkCV}/>
+    <PortfolioItem itemTitle={"React"} imageSrc={imageReact} link={linkReact} />
+    <PortfolioItem
+      itemTitle={"Web"}
+      imageSrc={imageFeatherandfox}
+      link={linkFeatherandfox}
+    />
+    <PortfolioItem itemTitle={"App"} imageSrc={imageApp} link={linkApp} />
+    <PortfolioItem
+      itemTitle={"Papers"}
+      imageSrc={imagePapers}
+      link={linkPapers}
+    />
+    <PortfolioItem
+      itemTitle={"D&D"}
+      imageSrc={imageDndPublications}
+      link={linkDnd}
+    />
+    <PortfolioItem itemTitle={"Poems"} imageSrc={imagePoems} link={linkPoems} />
+    <PortfolioItem
+      itemTitle={"Itch"}
+      imageSrc={imageMidjourney}
+      link={linkMidjourney}
+    />
+    <PortfolioItem itemTitle={"Resumé"} imageSrc={imageCV} link={linkCV} />
   </div>
 
   <div class="bottom">
     <a href="/">Go back to home page</a>
   </div>
-
 </main>
 
-
 <style>
-
-
-  .center{
-  flex-grow: 1;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  align-content: space-around;
+  .center {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-content: space-around;
   }
-  a{
-    color: var(--colour-grey)
+  a {
+    color: var(--colour-grey);
   }
-
 </style>
